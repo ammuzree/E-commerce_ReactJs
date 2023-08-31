996ecefd4fb89bed8ea7371556008360b3f5a8fe
ammuzree <ammuzree@gmail.com>
2023-08-31 17:47:45 +0530

    navBar creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import Header from './components/header';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from './components/Home';
+import Shope from './components/Shope';
+import About from './components/About';
+import Contact from './components/Contact'
+import Cart from './components/Cart';
+import Error from './components/Error'
+
 
 const App = ()=>{
-  <>
-  <div>
-    <Header/>
-  </div>
-  </>
+  return(<>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="shope" element={<Shope/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="contact" element={<Contact/>}/>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="*" element={<Error/>}/>
+
+    </Routes>
+    </BrowserRouter>
+    
+
+
+    </>);
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,38 @@
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = ()=>{
+const [togl, setTogl] = useState(false);
+
 return(
     <>
    
-    <nav>
-        <ul>
-            <li>     
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to='/shop'>Shop</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <li>
-             <Link to="/cart">Cart</Link>
-            </li>
+    <nav className='Nav'>
+        <h1 className='navLogo'>Logo</h1>
+        <ul className={togl ? 'navTogl':'nav-list'} onClick={()=>{setTogl(false)}}>
+           <Link to="/" className='home'>
+            <li>Home</li>
+            </Link>
+           
+              <Link to='/shope' className='shope'> 
+              <li>Shop </li>
+              </Link>
+
+              <Link to="/about" className='about'>
+                <li>About </li>
+                </Link>
+                     
+              <Link to="/contact" className='contact'>
+                <li>Contact</li>
+                </Link>       
+            
+             <Link to="/cart" className='cart'>
+              <li>Cart</li>
+              </Link>
+           
         </ul>
+        <button className='togleBtn' onClick={()=>{setTogl(!togl)}}>{togl ? <h1>X</h1> : <h1>=</h1>}</button>
     </nav>
 
 
